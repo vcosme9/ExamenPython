558995a6c1630aae0e8415222d93f6273d501cae
Vicent Cosme <vicentcosme6@gmail.com>
2021-03-24 10:56:30 +0100

    checkpoint 2

--- Examen.py
+++ Examen.py
@@ -1,8 +1,10 @@
+import Libro
 
+#datos de libros inventados
+libro_1 = Libro("Cervantes", "Don Quijote", 1950)
 
 
 def get_list (fichero):
-
     with fichero open as f
 
     palabras = []
@@ -15,30 +17,54 @@ def get_list (fichero):
     p_7 = []
     p_8 = []
     p_9 = []
+
     diccionario = {}
 
-    for lineas in f:
+    try:
+        for lineas in f:
         palabras.extend(lineas.split())
 
-    for i in range(len(palabras)):
-        if(len(palabras[i]) == 1):
-            p_1.append(palabras[i])
+        for i in range(len(palabras)):
+            if(len(palabras[i]) == 1):
+                p_1.append(palabras[i])
 
-        if(len(palabras[i]) == 2):
-            p_2.append(palabras[i])
+            if(len(palabras[i]) == 2):
+                p_2.append(palabras[i])
         
-        if(len(palabras[i]) == 3):
-            p_3.append(palabras[i])
+            if(len(palabras[i]) == 3):
+                p_3.append(palabras[i])
 
-        if(len(palabras[i]) == 4):
-            p_4.append(palabras[i])
+            if(len(palabras[i]) == 4):
+                p_4.append(palabras[i])
         
-        if(len(palabras[i]) == 5):
-            p_5.append(palabras[i])
+            if(len(palabras[i]) == 5):
+                p_5.append(palabras[i])
 
-        if(len(palabras[i]) == 6):
-            p_6.append(palabras[i])
+            if(len(palabras[i]) == 6):
+                p_6.append(palabras[i])
 
-        if(len(palabras[i]) == 7):
-            p_7.append(palabras[i])
+            if(len(palabras[i]) == 7):
+                p_7.append(palabras[i])
+
+            if(len(palabras[i]) == 8):
+                p_8.append(palabras[i])
+
+            if(len(palabras[i]) == 9):
+                p_9.append(palabras[i])
+    
+        diccionario[1] = p_1
+        diccionario[2] = p_2
+        diccionario[3] = p_3
+        diccionario[4] = p_4
+        diccionario[5] = p_5
+        diccionario[6] = p_6
+        diccionario[7] = p_7
+        diccionario[8] = p_8
+        diccionario[9] = p_9
+
+        return diccionario
+    except:
+        print("ValueError: el fichero esta vacio")
+
+def mas_antiguos(lista, anyo):
     
---
+++ libro.py
@@ -0,0 +1,7 @@
+
+
+class Libro:
+    def __init__(self, autor, titulo, anyo):
+    self.autor = autor
+    self.titulo = titulo
+    self.anyo = anyo
